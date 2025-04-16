cd84af8fe8fe9d5993f64aeef60e977825800117
haderrenteria13 <haderrenteria13az@gmail.com>
2025-04-16 12:08:28 -0500

    🔒️ Add private routes

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
-const PrivateRoutes = () => {
-  return (
-    <div>PrivateRoutes</div>
-  )
+const PrivateRoutes = ({ children }) => {
+  const token = localStorage.getItem('token')
+  return token ? children: <Navigate to="/login"/>
 }
 
 export default PrivateRoutes
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import LandingPage from "../features/landing/LandingPage";
-import NotFound from "../features/notFound/NotFound";
+import Auth from "../features/auth/Auth";
 import Dashboard from "../features/dashboard/Dashboard";
 import Categories from "../features/dashboard/modules/categories/Categories";
 import Products from "../features/dashboard/modules/products/Products";
 import Roles from "../features/dashboard/modules/roles/Roles";
 import Companies from "../features/dashboard/modules/companies/Companies";
+import PrivateRoutes from "./PrivateRoutes";
+import NotFound from "../features/notFound/NotFound";
 
 const Routes = createBrowserRouter([
     {
@@ -18,8 +20,16 @@ const Routes = createBrowserRouter([
         element: <LandingPage />
     },
     {
+        path: "/login",
+        element: <Auth/>
+    },
+    {
         path: "/dashboard",
-        element: <Dashboard />,
+        element: (
+            <PrivateRoutes>
+                <Dashboard />
+            </PrivateRoutes>
+        ),
         children: [
             {
                 path: "products",
